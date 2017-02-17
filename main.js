5e5cb130e624f3b89446992235e46ac52aff91e2
INNOSOFT\wiroon.sri <wiroon.sri@innosoft.kmutt.ac.th>
2017-02-18 00:10:19 +0700

    Create Windows Function

---
+++ main.js
@@ -0,0 +1,13 @@
+const {app, BrowserWindow} = require('electron')
+const url = require('url')
+const path = require('path')
+
+let win
+
+function createWindow ()
+{
+	win = new BrowserWindow()
+
+	
+	
+}

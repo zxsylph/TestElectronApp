b3d384ecc02de6668eb94c4a8869e7025d2aa138
INNOSOFT\wiroon.sri <wiroon.sri@innosoft.kmutt.ac.th>
2017-02-18 02:14:24 +0700

    Fix code using eslint

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
-const {app, BrowserWindow} = require('electron')
-const url = require('url')
-const path = require('path')
-const {ipcMain} = require('electron')
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+const {ipcMain} = require('electron');
 
-let win
+let win;
 
 function createWindow () {
-	win = new BrowserWindow({width:800, height:600})
+	win = new BrowserWindow({width:800, height:600});
 	win.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
 		protocol:'file',
 		slashes: true
-	}))
+	}));
 }
 
 ipcMain.on('openFile', (event, path)=>{
-	const {dialog} = require('electron')
-	const fs = require('fs')
+	const {dialog} = require('electron');
+	const fs = require('fs');
 
 	dialog.showOpenDialog(function (filenames) {
 		if (filenames === undefined) {
-			console.log('No Selected File(s)')
+			console.log('No Selected File(s)');
 		}
 		else {
-			readFile(filenames[0])
+			readFile(filenames[0]);
 		}
-	})
+	});
 
 	function readFile (filepath) {
 		fs.readFile(filepath, 'utf-8', (err, data)=>{
 			if (err) {
-				console.log(err.message)
-				return
+				console.log(err.message);
+				return;
 			}
-			event.sender.send('filename', data)
-		})
+			event.sender.send('filename', data);
+		});
 	}
-})
+});
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);

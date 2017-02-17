a955d5cf94297e6692e0164b17c1d2d44d8d8094
INNOSOFT\wiroon.sri <wiroon.sri@innosoft.kmutt.ac.th>
2017-02-18 00:55:31 +0700

    First complete electron app

--- main.js
+++ main.js
@@ -6,8 +6,12 @@ let win
 
 function createWindow ()
 {
-	win = new BrowserWindow()
-
-	
-	
+	win = new BrowserWindow({width:800, height:600})
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol:'file',
+		slashes: true
+	}))
 }
+
+app.on('ready', createWindow)

6e4406c9e9c8750d0d04db93e027ab025f13417e
INNOSOFT\wiroon.sri <wiroon.sri@innosoft.kmutt.ac.th>
2017-02-18 01:40:11 +0700

    using open file dialog

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const {app, BrowserWindow} = require('electron')
 const url = require('url')
 const path = require('path')
+const {ipcMain} = require('electron')
 
 let win
 
-function createWindow ()
-{
+function createWindow () {
 	win = new BrowserWindow({width:800, height:600})
 	win.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
@@ -13,5 +13,29 @@ function createWindow ()
 		slashes: true
 	}))
 }
+
+ipcMain.on('openFile', (event, path)=>{
+	const {dialog} = require('electron')
+	const fs = require('fs')
+
+	dialog.showOpenDialog(function (filenames) {
+		if (filenames === undefined) {
+			console.log('No Selected File(s)')
+		}
+		else {
+			readFile(filenames[0])
+		}
+	})
+
+	function readFile (filepath) {
+		fs.readFile(filepath, 'utf-8', (err, data)=>{
+			if (err) {
+				console.log(err.message)
+				return
+			}
+			event.sender.send('filename', data)
+		})
+	}
+})
 
 app.on('ready', createWindow)
